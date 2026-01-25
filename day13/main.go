3ee2a96be71c385a8f92861a548ec4cb8ed9bdb4
DollarCat <949013481@qq.com>
2026-01-25 22:28:59 +0800

    Gin02

--- day13/main.go
+++ day13/main.go
@@ -6,15 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
 func main() {
 	// 创建一个默认的路由引擎
 	r := gin.Default()
+	//配置模版的文件
+	r.LoadHTMLGlob("templates/*")
 	//配置路由
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "值:%v", "你好gin")
-	})
-	r.GET("/news", func(c *gin.Context) {
-		c.String(http.StatusOK, "我是新闻页面 111")
 	})
 
 	r.POST("/add", func(c *gin.Context) {
@@ -28,6 +33,70 @@ func main() {
 	r.DELETE("/delete", func(c *gin.Context) {
 		c.String(200, "这是一个DELETE请求 用于删除数据")
 	})
+
+	r.GET("/json1", func(c *gin.Context) {
+		c.JSON(200, map[string]interface{}{
+			"success": true,
+			"msg":     "你好gin",
+		})
+	})
+
+	r.GET("/json2", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"success": true,
+			"msg":     "你好gin--22",
+		})
+	})
+
+	r.GET("/json3", func(c *gin.Context) {
+
+		/*
+			使用指针：
+			１、避免值拷贝（性能考虑）：如果结构体很大（有很多字段），值拷贝会消耗更多内存和时间。
+			指针只占用 8 字节（64位系统），效率更高。
+			２、go惯例，对于较大结构体，通常使用指针传递
+		*/
+		a := &Article{
+			Title:   "我是一个标题",
+			Desc:    "描述",
+			Content: "测试内容",
+		}
+		c.JSON(200, a)
+	})
+
+	//响应Jsonp请求
+	// http://localhost:8080/jsonp?callback=xxxx
+	// xxxx({"title":"我是一个标题-jsonp","desc":"描述","content":"测试内容"});
+	r.GET("/jsonp", func(c *gin.Context) {
+
+		a := &Article{
+			Title:   "我是一个标题-jsonp",
+			Desc:    "描述",
+			Content: "测试内容",
+		}
+		c.JSONP(200, a)
+	})
+
+	r.GET("/xml", func(c *gin.Context) {
+		c.XML(http.StatusOK, gin.H{
+			"success": true,
+			"msg":     "你好gin 我是一个xml",
+		})
+	})
+
+	r.GET("/news", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "news.html", gin.H{
+			"title": "我是后台的数据",
+		})
+	})
+
+	r.GET("/goods", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "goods.html", gin.H{
+			"title": "我是商品页面",
+			"price": 20,
+		})
+	})
+
 	// r.Run()  启动 HTTP 服务，默认在 0.0.0.0:8080 启动服务
 
 	r.Run(":8000") //启动一个web服务
